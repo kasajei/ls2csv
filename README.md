7d44deffaa98d72d8babaab96877c3c59b2c7557
kasajei@retina <sevenbluesea.yk@gmail.com>
2013-05-30 18:37:41 +0900

    add comment format

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 - `ruby ls2csv.rb`
 - generete Localizable.txt that is csv format file.
 - You open this gerenrated file in GoogleDocs!
+
+## Comment format
+If you comment on Localizable.string with below format, you can save your comment
+
+```
+// MARK: - ViewController
+"hogehogeViewController.ok.button" = "OK"; // save button
+```
+
--- forUTF-8/ls2csv.rb
+++ forUTF-8/ls2csv.rb
@@ -4,34 +4,44 @@ require "kconv"
 # please set defaults language folder
 localizedFolderName = "ja.lproj"
 File::open(localizedFolderName + "/Localizable.strings") do |f|
-  CSV.open("localizable.txt","w") do |csv|
-    csv << ["key","ja","en","for system",'=""""&$A1&""""&"="&""""&B1&""";"']
-    keyAry = []
-    f.each do |line|
-      if /^"/ =~ line
-        lineAry = line.split(/\"/)
-        splitFlag = 0
-        keysStr = ""
-        wordsStr= ""
-        lineAry.each do |word|
-          if splitFlag == 0 
-            if /=/ =~ word
-              splitFlag = 1
-            else
-              keysStr += word
-            end
-          else
-            if /;/ =~ word
-              break
-            end
-            wordsStr += word
-          end
-        end
+	CSV.open("localizable.txt","w") do |csv|
+		csv << ["ViewController","LineComment","key","ja","en","for system",'=IF(ISBLANK($C1),$A1,""""&$C1&""""&"="&""""&D1&""";"&$B1)']
+		keyAry = []
+		f.each do |line|
+      if /^\/\// =~ line # this line is comment
         cell = []
-        cell << keysStr
-        cell << wordsStr
         csv << cell
-      end 
-    end
-  end	
+        cell << line
+        csv << cell
+      end
+			if /^"/ =~ line # this line is localizable string
+				lineAry = line.split(/\"/)
+				splitFlag = 0
+				keysStr = ""
+				wordsStr = ""
+        commentStr = ""
+				lineAry.each do |word|
+					if splitFlag == 0 
+						if /=/ =~ word
+							splitFlag = 1
+						else
+							keysStr += word
+						end
+          else 
+						if /;/ =~ word
+              commentStr += word.delete(";")
+						else
+						  wordsStr += word
+            end
+					end
+				end
+				cell = []
+        cell << ""
+        cell << commentStr
+				cell << keysStr
+				cell << wordsStr
+				csv << cell
+			end 
+		end
+	end	
 end
