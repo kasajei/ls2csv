61af7333a1643efbf8e39db5780dd2e4b35dbd8d
kasajei@retina <sevenbluesea.yk@gmail.com>
2013-05-30 19:35:30 +0900

    add chomp

--- forUTF-8/ls2csv.rb
+++ forUTF-8/ls2csv.rb
@@ -4,44 +4,46 @@ require "kconv"
 # please set defaults language folder
 localizedFolderName = ""
 File::open(localizedFolderName + "Localizable.strings") do |f|
-	CSV.open("localizable.txt","w") do |csv|
-		csv << ["ViewController","// LineComment","key","ja","en","for system",'=IF(ISBLANK($C1),$A1,""""&$C1&""""&"="&""""&D1&""";"&$B1)']
-		keyAry = []
-		f.each do |line|
+  CSV.open("localizable.txt","w") do |csv|
+    csv << ["ViewController","// LineComment","key","ja","en","for system",'=IF(ISBLANK($C1),$A1,""""&$C1&""""&"="&""""&D1&""";"&$B1)']
+    keyAry = []
+    f.each do |line|
       if /^\/\// =~ line # this line is comment
+        csv << [""]
         cell = []
-        csv << cell
-        cell << line
+        cell << line.chomp
         csv << cell
       end
-			if /^"/ =~ line # this line is localizable string
-				lineAry = line.split(/\"/)
-				splitFlag = 0
-				keysStr = ""
-				wordsStr = ""
+      if /^"/ =~ line # this line is localizable string
+        lineAry = line.split(/\"/)
+        splitFlag = 0
+        keysStr = ""
+        wordsStr = ""
         commentStr = ""
-				lineAry.each do |word|
-					if splitFlag == 0 
-						if /=/ =~ word
-							splitFlag = 1
-						else
-							keysStr += word
-						end
-          else 
-						if /;/ =~ word
-              commentStr += word.delete(";")
-						else
-						  wordsStr += word
+        lineAry.each do |word|
+          if splitFlag == 0 
+            if /=/ =~ word
+              splitFlag = 1
+            else
+              keysStr += word
             end
-					end
-				end
-				cell = []
+          else 
+            if /;/ =~ word
+              if /\/\// =~ word
+                commentStr += word.delete(";").chomp
+              end
+            else
+              wordsStr += word
+            end
+          end
+        end
+        cell = []
         cell << ""
         cell << commentStr
-				cell << keysStr
-				cell << wordsStr
-				csv << cell
-			end 
-		end
-	end	
+        cell << keysStr
+        cell << wordsStr
+        csv << cell
+      end 
+    end
+  end 
 end
